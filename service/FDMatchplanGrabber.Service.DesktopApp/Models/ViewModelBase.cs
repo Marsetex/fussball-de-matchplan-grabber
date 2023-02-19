a38daf2311e12c2e2701477ebe7fb3dd9f3309ce
Marsetex <marcel.gruessinger@gmail.com>
2023-02-19 23:44:26 +0100

    Implemented view models

---
+++ service/FDMatchplanGrabber.Service.DesktopApp/Models/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace FDMatchplanGrabber.Service.DesktopApp.Models
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
